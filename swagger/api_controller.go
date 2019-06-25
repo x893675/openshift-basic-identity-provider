8b8602983a24cc4e692336e22e7b600471c40b6b
x893675 <x893675@163.com>
2019-06-25 13:50:17 +0800

    fix bug

--- swagger/api_controller.go
+++ swagger/api_controller.go
@@ -137,7 +137,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	userinfo.Password=""
-	userinfo.Sub = userinfo.ID
+	userinfo.Sub = string(userinfo.ID)
 	userinfo.PreferredUsername = userinfo.Name
 	userinfo.Username = ""
 	w.WriteHeader(http.StatusOK)
